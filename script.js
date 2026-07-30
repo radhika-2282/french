3fff54e536d337d18c8ed4591078fbd180fbf4f0
radhika-2282 <radhika.t@hyd.silveroaks.co.in>
2026-07-30 16:31:53 +0000

    add emoji font

--- script.js
+++ script.js
@@ -3,20 +3,29 @@ function speak(word) {
     let fileHash = md5(word);
     // 1. Construct the path to the voice file
     // Assumes 'public' is the root directory of your web server
-    const audioPath = `/voices/${fileHash}.mp3`;
+    const audioPath = `./voices/${fileHash}.mp3`;
 
     // 2. Create a new Audio object and play it
     const audio = new Audio(audioPath);
-
-    audio.play().catch(error => {
-        console.error(`Failed to play audio for hash [${fileHash}]:`, error);
-        speakWithBrowserVoice(word);
+    audio.play().catch(e => {
+        if(e.code == 9) {
+            speakWithBrowserVoice(word)
+        } else if (e.code == 0) {
+            console.log("user didn't interact with page")
+        } else {
+            console.error(`failed to play = ${word}`)
+            console.error(e)
+        }
     });
+        
 }
+
+    
 
 function speakWithBrowserVoice(text) {
     if (!('speechSynthesis' in window)) return;
-
+    
+    console.warn("Fatal! using browser fallback")
     const utterance = new SpeechSynthesisUtterance(text);
     utterance.lang = 'fr-FR';
     utterance.rate = 0.95;
